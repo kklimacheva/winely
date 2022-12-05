075659c6bf76959a9286b8da7dee60a894a5bd32
kklimacheva <kateklimacheva02@gmail.com>
2022-12-05 14:09:35 +0300

    fix: minor fixes

--- js/customList.js
+++ js/customList.js
@@ -40,22 +40,22 @@ function clearStorage(){
 
 function displayToBuyList(toBuyArr){
     toBuyList.innerHTML = '';
-    toBuyArr.forEach(function (todo) {
+    toBuyArr.forEach(function (toBuy) {
         const div = document.createElement("div");
         div.setAttribute("class", "toBuyList__item")
-        div.setAttribute("id", todo.id);
+        div.setAttribute("id", toBuy.id);
         div.innerHTML = `
-            <input type="checkbox" class="checkbox ${todo.id}">
-            ${todo.wine}
+            <input type="checkbox" class="checkbox ${toBuy.id}">
+            ${toBuy.wine}
             <button class="delete__button">Delete</button>
         `;
         toBuyList.append(div);
     })
 }
 
-function deleteTodo(id){
-    toBuyArray = toBuyArray.filter(function (todo) {
-        return todo.id !== id;
+function deleteToBuy(id){
+    toBuyArray = toBuyArray.filter(function (toBuy) {
+        return toBuy.id !== id;
     });
     store(toBuyArray);
 }
@@ -65,6 +65,6 @@ getFromStorage();
 toBuyList.addEventListener('click', (event) => {
     const isButton = event.target.classList.contains("delete__button");
     if (isButton){
-        deleteTodo(event.target.parentElement.id);
+        deleteToBuy(event.target.parentElement.id);
     }
 })
